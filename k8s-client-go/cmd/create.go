9dc835475058dbe934079ef9876770d24435a100
nightfury1204 <nahid@appscode.com>
2017-12-14 18:27:46 +0600

    service annotation added using patch

--- k8s-client-go/cmd/create.go
+++ k8s-client-go/cmd/create.go
@@ -16,17 +16,24 @@ package cmd
 
 import (
 	"log"
+	"encoding/json"
+	"fmt"
 
 	"github.com/spf13/cobra"
 	appsv1beta2 "k8s.io/api/apps/v1beta2"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 	typedappsv1beta2 "k8s.io/client-go/kubernetes/typed/apps/v1beta2"
 	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/apimachinery/pkg/util/intstr"
-	"fmt"
+	"k8s.io/apimachinery/pkg/util/strategicpatch"
+	"github.com/golang/glog"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+	kerr "k8s.io/apimachinery/pkg/api/errors"
+	"time"
 )
 
 var Kubeconfig string //contain Kubeconfig file path
@@ -55,17 +62,25 @@ var CreateCmd = &cobra.Command{
 
 		serviceClient := clientset.CoreV1().Services(apiv1.NamespaceDefault)
 		service, err := createService(serviceClient)
-		if err!= nil {
+		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("Service created : %q\n",service.GetObjectMeta().GetName())
+		log.Printf("Service created : %q\n", service.GetObjectMeta().GetName())
 
-		node, err := clientset.CoreV1().Nodes().Get("minikube",metav1.GetOptions{})
-		if err!=nil {
+		node, err := clientset.CoreV1().Nodes().Get("minikube", metav1.GetOptions{})
+		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println("Access Url:")
-		fmt.Printf("%v:%v\n",node.Status.Addresses[0].Address,service.Spec.Ports[0].NodePort)
+		fmt.Printf("%v:%v\n", node.Status.Addresses[0].Address, service.Spec.Ports[0].NodePort)
+
+		//add annotation using patch
+		tryPatchService(clientset,service.ObjectMeta,func(service *apiv1.Service) *apiv1.Service{
+			service.SetAnnotations(map[string] string {
+				"reason":"patch test",
+			})
+			return service
+		})
 	},
 }
 
@@ -116,7 +131,7 @@ func createDeployment(deploymentsClient typedappsv1beta2.DeploymentInterface) (*
 	return result, err
 }
 
-func createService(serviceClient typedcorev1.ServiceInterface) ( *apiv1.Service, error) {
+func createService(serviceClient typedcorev1.ServiceInterface) (*apiv1.Service, error) {
 	service := &apiv1.Service{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "hello-service",
@@ -128,12 +143,12 @@ func createService(serviceClient typedcorev1.ServiceInterface) ( *apiv1.Service,
 			},
 			Ports: []apiv1.ServicePort{
 				{
-					Name:       "http",
-					Protocol:   apiv1.ProtocolTCP,
-					Port:       8888,
+					Name:     "http",
+					Protocol: apiv1.ProtocolTCP,
+					Port:     8888,
 					TargetPort: intstr.IntOrString{
-						Type: intstr.Int,
-						IntVal:8080,
+						Type:   intstr.Int,
+						IntVal: 8080,
 					},
 				},
 			},
@@ -143,7 +158,50 @@ func createService(serviceClient typedcorev1.ServiceInterface) ( *apiv1.Service,
 	log.Println("Creating service...")
 	result, err := serviceClient.Create(service)
 
-	return result,err
+	return result, err
+}
+
+func PatchService(c kubernetes.Interface, cur *apiv1.Service, transform func(*apiv1.Service) *apiv1.Service) (*apiv1.Service, error) {
+	curJson, err := json.Marshal(cur)
+	if err != nil {
+		return nil, err
+	}
+
+	modJson, err := json.Marshal(transform(cur.DeepCopy()))
+	if err != nil {
+		return nil, err
+	}
+
+	patch, err := strategicpatch.CreateTwoWayMergePatch(curJson, modJson, apiv1.Service{})
+	if err != nil {
+		return nil, err
+	}
+	if len(patch) == 0 || string(patch) == "{}" {
+		return cur, nil
+	}
+	glog.V(3).Infof("Patching Service %s/%s with %s.", cur.Namespace, cur.Name, string(patch))
+	return c.CoreV1().Services(cur.Namespace).Patch(cur.Name, types.StrategicMergePatchType, patch)
+}
+
+func tryPatchService(c kubernetes.Interface, meta metav1.ObjectMeta, transform func(* apiv1.Service) *apiv1.Service) (result *apiv1.Service, err error) {
+	attempt := 0
+	err = wait.PollImmediate(1*time.Second, 5*time.Second, func() (bool, error) {
+		attempt++
+		cur, e2 := c.CoreV1().Services(meta.Namespace).Get(meta.Name, metav1.GetOptions{})
+		if kerr.IsNotFound(e2) {
+			return false, e2
+		} else if e2 == nil {
+			result, e2 = PatchService(c, cur, transform)
+			return e2 == nil, nil
+		}
+		glog.Errorf("Attempt %d failed to patch Service %s/%s due to %v.", attempt, cur.Namespace, cur.Name, e2)
+		return false, nil
+	})
+
+	if err != nil {
+		err = fmt.Errorf("failed to patch Service %s/%s after %d attempts due to %v", meta.Namespace, meta.Name, attempt, err)
+	}
+	return
 }
 
 func int32Ptr(i int32) *int32 {
