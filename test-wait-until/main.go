d25be807f46e27df140d74abb23fd7739675de96
nightfury1204 <nahid@appscode.com>
2018-08-02 15:09:35 +0600

    Add wait.until example

--- test-wait-until/main.go
+++ test-wait-until/main.go
@@ -1,1 +1,21 @@
-package test_wait_until
+package main
+
+import (
+	"k8s.io/apimachinery/pkg/util/wait"
+	"fmt"
+	"time"
+)
+
+func main() {
+	shutdown := make(chan struct {})
+	go wait.Until(print, time.Second*2, shutdown)
+
+	select {
+	case <-time.After(time.Second * 10):
+		close(shutdown)
+	}
+}
+
+func print() {
+	fmt.Println(time.Now().Unix())
+}
