595c34ffe6ab7716e2d1620ef6a5ceec4f751bd2
nightfury1204 <nahid@appscode.com>
2018-08-08 11:06:46 +0600

    Add interface example

---
+++ interface/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type AInterface interface {
+	Hello()
+}
+
+type BInterface interface {
+	AInterface
+	Bye()
+}
+
+
+type A struct {
+	greetings string
+}
+
+func NewA() *A {
+	return &A{
+		greetings:"Good morning",
+	}
+}
+
+func (a *A) Hello() {
+	fmt.Println(a.greetings)
+}
+
+type B struct {
+	AInterface
+	goodBye string
+}
+
+func NewB(a AInterface) *B {
+	return &B{
+		AInterface:a,
+		goodBye: "Goodbye",
+	}
+}
+
+func (b *B) Bye() {
+	fmt.Println(b.goodBye)
+}
+
+func main()  {
+	var a AInterface
+	a = NewA()
+
+	var b BInterface
+	b = NewB(a)
+
+	b.Hello()
+	b.Bye()
+}
+
