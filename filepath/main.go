d89ba44b6d39e02c1cd8d2336980065417c4a4df
nightfury1204 <nahid@appscode.com>
2018-10-23 16:10:07 +0600

    test filepath

---
+++ filepath/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"path/filepath"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(filepath.Clean("hi/"))
+	fmt.Println(filepath.Clean("hi"))
+	fmt.Println(filepath.Clean("//hi//"))
+	fmt.Println(filepath.Clean("/hi//"))
+	fmt.Println(filepath.Clean("hello/hi//"))
+
+	fmt.Println(filepath.Join("hi","/"))
+	fmt.Println(filepath.Join("hi/","/"))
+}
