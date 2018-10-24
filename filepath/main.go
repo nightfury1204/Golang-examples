d8a3b94cae1b137807e28375bc56930ebd9be5d8
nightfury1204 <nahid@appscode.com>
2018-10-24 10:08:39 +0600

    test filepath

--- filepath/main.go
+++ filepath/main.go
@@ -12,6 +12,9 @@ func main() {
 	fmt.Println(filepath.Clean("/hi//"))
 	fmt.Println(filepath.Clean("hello/hi//"))
 
+
+	fmt.Println(filepath.Clean("hello/hi//")+"/")
+
 	fmt.Println(filepath.Join("hi","/"))
 	fmt.Println(filepath.Join("hi/","/"))
 }
