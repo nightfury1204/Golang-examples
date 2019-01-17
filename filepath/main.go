72d9100fab7fc38240165c51384257b92687ace7
nightfury1204 <nahid@appscode.com>
2019-01-17 14:39:23 +0600

    Add url example

--- filepath/main.go
+++ filepath/main.go
@@ -12,9 +12,11 @@ func main() {
 	fmt.Println(filepath.Clean("/hi//"))
 	fmt.Println(filepath.Clean("hello/hi//"))
 
-
 	fmt.Println(filepath.Clean("hello/hi//")+"/")
 
 	fmt.Println(filepath.Join("hi","/"))
 	fmt.Println(filepath.Join("hi/","/"))
+
+	fmt.Println(filepath.Join("hi","hello"))
+	fmt.Println(filepath.Join("hi/","/hello"))
 }
